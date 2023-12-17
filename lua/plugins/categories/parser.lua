a3ff4a184596eef2f01c14eb29582ca157582717
senkentarou <masahiro-senda@c-fo.com>
2023-12-17 15:57:00 +0900

    splits hop

--- lua/plugins/categories/parser.lua
+++ lua/plugins/categories/parser.lua
@@ -19,23 +19,6 @@ return {
     opts = {},
   },
   {
-    'smoka7/hop.nvim',
-    version = '*',
-    init = function()
-      vim.api.nvim_create_autocmd({
-        "VimEnter",
-        "ColorScheme",
-      }, {
-        group = vim.api.nvim_create_augroup("MyHop", {
-          clear = true,
-        }),
-        pattern = "*",
-        command = "highlight HopUnmatched cterm=underline ctermfg=242 gui=underline guifg=#666666 guisp=#666666",
-      })
-    end,
-    opts = {},
-  },
-  {
     'numToStr/Comment.nvim',
     event = 'BufRead',
     opts = {},
---
+++ lua/plugins/individuals/hop.lua
@@ -0,0 +1,19 @@
+return {
+  {
+    'smoka7/hop.nvim',
+    version = '*',
+    init = function()
+      vim.api.nvim_create_autocmd({
+        "VimEnter",
+        "ColorScheme",
+      }, {
+        group = vim.api.nvim_create_augroup("MyHop", {
+          clear = true,
+        }),
+        pattern = "*",
+        command = "highlight HopUnmatched cterm=underline ctermfg=242 gui=underline guifg=#666666 guisp=#666666",
+      })
+    end,
+    opts = {},
+  },
+}
