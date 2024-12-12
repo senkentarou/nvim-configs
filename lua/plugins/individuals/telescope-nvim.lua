6d949e73fe100c7ee7828aea26954615048da0f4
senkentarou <masahiro-senda@c-fo.com>
2024-12-12 22:18:27 +0900

    telescopeのtag指定をなくす

--- lua/plugins/individuals/telescope-nvim.lua
+++ lua/plugins/individuals/telescope-nvim.lua
@@ -221,7 +221,6 @@ end
 return {
   {
     'nvim-telescope/telescope.nvim',
-    tag = '0.1.5',
     dependencies = {
       'nvim-lua/plenary.nvim',
       'nvim-telescope/telescope-file-browser.nvim',
